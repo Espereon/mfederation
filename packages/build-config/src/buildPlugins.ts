7f25b4382a450a8da40947826c19357b9a2cea6a
Espereon <announcement32@post.com>
2025-07-25 17:51:00 +0300

    init

---
+++ packages/build-config/src/buildPlugins.ts
@@ -0,0 +1,50 @@
+import { Configuration, DefinePlugin } from "webpack";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import path from "path";
+export function buildPlugins({
+  mode,
+  paths,
+  analyzer,
+  platform,
+}: BuildOptions): Configuration["plugins"] {
+  // c использование деструктуризации
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+  const plugins: Configuration["plugins"] = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+      favicon: path.resolve(paths.public, "test.ico"), // Настройки ссылки на файл с html документом
+    }),
+    new DefinePlugin({
+      __PLATFORM__: JSON.stringify(platform),
+      __ENV__: JSON.stringify(mode),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+    plugins.push(new ForkTsCheckerWebpackPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+  }
+
+  if (analyzer) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return plugins;
+}
